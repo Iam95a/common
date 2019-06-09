f4a4ea90689befb59186334437c56b9e4da62b2b
goldGreatChen <chenjinwei1109@gmail.com>
2019-06-10 00:24:01 +0800

    im聊天记录

--- src/main/java/com/chen/common/im/ImServer.java
+++ src/main/java/com/chen/common/im/ImServer.java
@@ -14,6 +14,7 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.protobuf.ProtobufDecoder;
 import io.netty.handler.codec.protobuf.ProtobufEncoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
 
 import java.net.InetSocketAddress;
 import java.util.Map;
@@ -30,39 +31,39 @@ public class ImServer {
                 .localAddress(new InetSocketAddress(8888))
                 .childHandler(new ChannelInitializer<SocketChannel>() {
                     @Override
-                    protected void initChannel(SocketChannel socketChannel) throws Exception {
-                        socketChannel.pipeline()
-                                .addLast(new ProtobufVarint32FrameDecoder())
-                                .addLast(new ProtobufEncoder())
-                                .addLast(new ProtobufDecoder(SingleMessageProto.SingleMessage.getDefaultInstance()))
-                                .addLast(new ChannelInboundHandlerAdapter() {
-                                    @Override
-                                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                        if (msg instanceof SingleMessageProto.SingleMessage) {
-                                            SingleMessageProto.SingleMessage singleMessage = (SingleMessageProto.SingleMessage) msg;
-                                            if (singleMessage.getType() == TypeEnum.LOGIN.getCode()) {
-                                                User user = new User();
-                                                user.setUserId(1);
-                                                user.setNickname(singleMessage.getNickname());
-                                                map.put(user.getUserId(), user);
-                                                ctx.writeAndFlush(getReturnMessage(singleMessage, user));
-                                            }
-                                        } else {
-                                            System.out.println(msg);
-                                        }
+                    protected void initChannel(SocketChannel sc) throws Exception {
+                        //protobuf的顺序还是蛮重要的  具体为什么需要查一下资料
+                        sc.pipeline().addLast(new ProtobufVarint32FrameDecoder());
+                        sc.pipeline().addLast(new ProtobufDecoder(SingleMessageProto.SingleMessage.getDefaultInstance()));
+                        sc.pipeline().addLast(new ProtobufVarint32LengthFieldPrepender());
+                        sc.pipeline().addLast(new ProtobufEncoder());
+                        sc.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                if (msg instanceof SingleMessageProto.SingleMessage) {
+                                    SingleMessageProto.SingleMessage singleMessage = (SingleMessageProto.SingleMessage) msg;
+                                    if (singleMessage.getType() == TypeEnum.LOGIN.getCode()) {
+                                        User user = new User();
+                                        user.setUserId(1);
+                                        user.setNickname(singleMessage.getNickname());
+                                        map.put(user.getUserId(), user);
+                                        ctx.writeAndFlush(getReturnMessage(singleMessage, user));
                                     }
+                                } else {
+                                    System.out.println(msg);
+                                }
+                            }
 
 
+                            private SingleMessageProto.SingleMessage getReturnMessage(SingleMessageProto.SingleMessage singleMessage, User user) {
+                                SingleMessageProto.SingleMessageOrBuilder builder = SingleMessageProto.SingleMessage.newBuilder();
+                                ((SingleMessageProto.SingleMessage.Builder) builder).setMsgId(singleMessage.getMsgId());
+                                ((SingleMessageProto.SingleMessage.Builder) builder).setUserId(user.getUserId());
+                                ((SingleMessageProto.SingleMessage.Builder) builder).setType(TypeEnum.LOGIN.getCode());
+                                return ((SingleMessageProto.SingleMessage.Builder) builder).build();
+                            }
 
-                                    private SingleMessageProto.SingleMessage getReturnMessage(SingleMessageProto.SingleMessage singleMessage, User user) {
-                                        SingleMessageProto.SingleMessageOrBuilder builder = SingleMessageProto.SingleMessage.newBuilder();
-                                        ((SingleMessageProto.SingleMessage.Builder) builder).setMsgId(singleMessage.getMsgId());
-                                        ((SingleMessageProto.SingleMessage.Builder) builder).setUserId(user.getUserId());
-                                        ((SingleMessageProto.SingleMessage.Builder) builder).setType(TypeEnum.LOGIN.getCode());
-                                        return ((SingleMessageProto.SingleMessage.Builder) builder).build();
-                                    }
-
-                                });
+                        });
                     }
                 });
         ChannelFuture f = serverBootstrap.bind().sync();
